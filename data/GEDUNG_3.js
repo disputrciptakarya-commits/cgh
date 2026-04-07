a1209750b3dc6bd3d8547ae52219e118a978a14b
disputrciptakarya-commits <disputrciptakarya@gmail.com>
2026-04-08 03:09:51 +0700

    Add files via upload

---
+++ data/GEDUNG_3.js
@@ -0,0 +1,9 @@
+var json_GEDUNG_3 = {
+"type": "FeatureCollection",
+"name": "GEDUNG_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID_ASET": "DPW_GDN_001", "KELURAHAN": "MULIA BARU", "KECAMATAN": "DELTA PAWAN", "JALAN_GANG": "JL. JENDERAL SUDIRMAN", "VOLUME": null, "TAHUN": "2026", "PHOTO": "C:/Users/HYPE AMD/Pictures/KELOMPOK 1/07.PEMBANGUAN REHABILITASI GEDUNG KANTOR BAPPEDA KAB. KETAPANG_/IMG20260224082452.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.979021095646658, -1.839099124385999 ] } },
+{ "type": "Feature", "properties": { "ID_ASET": "DPW_GDG_002", "KELURAHAN": "SUKAHARJA", "KECAMATAN": "DELTA PAWAN", "JALAN_GANG": "JL. BRIGJEND KATAMSO", "VOLUME": null, "TAHUN": "2026", "PHOTO": "C:/Users/HYPE AMD/Pictures/KELOMPOK 1/08.PEMBANGUNAN TERAS BELAKANG PENDOPO RUMAH DINAS WAKIL BUPATI JL. BRIGJEN KATAMSO KELURAHAN SUKAHARJA KECAMATAN DELTA PAWAN_/1773043091002.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.979258895206186, -1.821914015561813 ] } }
+]
+}
